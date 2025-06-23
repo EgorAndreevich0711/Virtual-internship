ce3d6af7e56507049c93a7eb769b05d3078ef4a3
EgorAndreevich0711 <egorgrebennikov39@gmail.com>
2025-06-24 00:53:37 +0800

    Добавлено исправление / UpdateStatus для модерации

--- mountain_pass/views.py
+++ mountain_pass/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -21,3 +22,13 @@ class SubmitDataView(APIView):
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR
                 )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdatePassStatusView(APIView):
+    def patch(self, request, pass_id):
+        new_status = request.data.get('status')
+        try:
+            updated_pass = DatabaseManager.update_status(pass_id, new_status)
+            return Response({"new_status": updated_pass.status}, status=200)
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=400)
