3d44f8b7d13170266f2abbb82b3d7f1d67ebd3bc
EgorAndreevich0711 <egorgrebennikov39@gmail.com>
2025-06-24 00:53:37 +0800

    Добавлено исправление / UpdateStatus для модерации

--- mountain_pass/urls.py
+++ mountain_pass/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SubmitDataView
+from .views import SubmitDataView, UpdatePassStatusView
 
 urlpatterns = [
     path('submitData/', SubmitDataView.as_view(), name='submit-data'),
+    path('updateStatus/<int:pass_id>/', UpdatePassStatusView.as_view()),
 ]
