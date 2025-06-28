6bdaf44a254fc53e36bab7b03b83afd82862b2d6
EgorAndreevich0711 <egorgrebennikov39@gmail.com>
2025-06-29 04:17:07 +0800

    Обновление URL

--- mountain_pass/urls.py
+++ mountain_pass/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import SubmitDataView, UpdatePassStatusView
+from .views import SubmitDataView, UpdatePassStatusView, MountainPassDetail, MountainPassUpdate, UserMountainPassList
 
 urlpatterns = [
     path('submitData/', SubmitDataView.as_view(), name='submit-data'),
     path('updateStatus/<int:pass_id>/', UpdatePassStatusView.as_view()),
+
+    path('submitData/get/<int:pk>/', MountainPassDetail.as_view()),
+    path('submitData/update/<int:pk>/', MountainPassUpdate.as_view()),
+    path('submitData/list/', UserMountainPassList.as_view()),
 ]
