e498abe03534fbbe7e75487e7e5247d5a9e6d43e
EgorAndreevich0711 <egorgrebennikov39@gmail.com>
2025-06-29 04:45:58 +0800

    Небольшая добработка Name URL

--- mountain_pass/urls.py
+++ mountain_pass/urls.py
@@ -3,9 +3,9 @@ from .views import SubmitDataView, UpdatePassStatusView, MountainPassDetail, Mou
 
 urlpatterns = [
     path('submitData/', SubmitDataView.as_view(), name='submit-data'),
-    path('updateStatus/<int:pass_id>/', UpdatePassStatusView.as_view()),
+    path('updateStatus/<int:pass_id>/', UpdatePassStatusView.as_view(), name='update-status'),
 
-    path('submitData/get/<int:pk>/', MountainPassDetail.as_view()),
-    path('submitData/update/<int:pk>/', MountainPassUpdate.as_view()),
-    path('submitData/list/', UserMountainPassList.as_view()),
+    path('submitData/get/<int:pk>/', MountainPassDetail.as_view(), name='mountain_pass-detail'),
+    path('submitData/update/<int:pk>/', MountainPassUpdate.as_view(), name='mountain_pass-update'),
+    path('submitData/list/', UserMountainPassList.as_view(), name='mountain_pass-user-list'),
 ]
