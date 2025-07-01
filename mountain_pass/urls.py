596ca2a483ea8dfe80320350b6b7a9544b37f2ce
EgorAndreevich0711 <egorgrebennikov39@gmail.com>
2025-07-02 00:13:17 +0800

    Переделаны URL адреса

--- mountain_pass/urls.py
+++ mountain_pass/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import SubmitDataView, UpdatePassStatusView, MountainPassDetail, MountainPassUpdate, UserMountainPassList
+from .views import SubmitDataView,  UserMountainPassList, MountainPassDetailUpdate
 
 urlpatterns = [
-    path('submitData/', SubmitDataView.as_view(), name='submit-data'),
-    path('updateStatus/<int:pass_id>/', UpdatePassStatusView.as_view(), name='update-status'),
 
-    path('submitData/get/<int:pk>/', MountainPassDetail.as_view(), name='mountain_pass-detail'),
-    path('submitData/update/<int:pk>/', MountainPassUpdate.as_view(), name='mountain_pass-update'),
-    path('submitData/list/', UserMountainPassList.as_view(), name='mountain_pass-user-list'),
+    path('submitData/', SubmitDataView.as_view(), name='submit-data'),
+
+    path('submitData/<int:pk>/', MountainPassDetailUpdate.as_view(), name='mountainpass-detail-update'),
+
+
+    path('submitData/', UserMountainPassList.as_view(), name='mountainpass-user-list'),
 ]
