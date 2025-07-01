35eb7f77ce7c67824f5e50442165796bcfffb154
EgorAndreevich0711 <egorgrebennikov39@gmail.com>
2025-07-01 11:46:46 +0800

    Добаботка сериалайзеров

--- mountain_pass/serializers.py
+++ mountain_pass/serializers.py
@@ -1,5 +1,5 @@
-
 from rest_framework import serializers
+from django.utils.encoding import force_str
 from .models import User, Coords, Level, MountainPass, Image
 
 
@@ -7,6 +7,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'phone', 'fam', 'name', 'otc']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        # Принудительное преобразование текстовых полей
+        for field in ['fam', 'name', 'otc']:
+            if field in data and data[field]:
+                data[field] = force_str(data[field])
+        return data
 
 
 class CoordsSerializer(serializers.ModelSerializer):
@@ -16,15 +24,31 @@ class CoordsSerializer(serializers.ModelSerializer):
 
 
 class LevelSerializer(serializers.ModelSerializer):
+    winter = serializers.ChoiceField(choices=Level.LEVEL_CHOICES, required=False, allow_blank=True)
+    summer = serializers.ChoiceField(choices=Level.LEVEL_CHOICES, required=False, allow_blank=True)
+    autumn = serializers.ChoiceField(choices=Level.LEVEL_CHOICES, required=False, allow_blank=True)
+    spring = serializers.ChoiceField(choices=Level.LEVEL_CHOICES, required=False, allow_blank=True)
+
     class Meta:
         model = Level
         fields = ['winter', 'summer', 'autumn', 'spring']
+
+    def validate(self, data):
+        if not any(data.values()):
+            raise serializers.ValidationError("Хотя бы один сезон должен быть указан")
+        return data
 
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = ['title', 'image']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if 'title' in data:
+            data['title'] = force_str(data['title'])
+        return data
 
 
 class MountainPassSerializer(serializers.ModelSerializer):
@@ -41,18 +65,25 @@ class MountainPassSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id', 'add_time', 'status']
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        # Обрабатываем все текстовые поля
+        text_fields = ['beauty_title', 'title', 'other_titles', 'connect']
+        for field in text_fields:
+            if field in data and data[field]:
+                data[field] = force_str(data[field])
+        return data
+
     def create(self, validated_data):
         user_data = validated_data.pop('user')
         coords_data = validated_data.pop('coords')
         level_data = validated_data.pop('level')
         images_data = validated_data.pop('images')
 
-        # Создаем связанные объекты
         user = User.objects.create(**user_data)
         coords = Coords.objects.create(**coords_data)
         level = Level.objects.create(**level_data)
 
-        # Создаем перевал
         mountain_pass = MountainPass.objects.create(
             user=user,
             coords=coords,
@@ -61,7 +92,6 @@ class MountainPassSerializer(serializers.ModelSerializer):
             **validated_data
         )
 
-        # Создаем изображения
         for image_data in images_data:
             Image.objects.create(mountain_pass=mountain_pass, **image_data)
 
