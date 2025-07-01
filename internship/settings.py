3be195eeb1d26dc52bf0193484feaa09b1a7365f
EgorAndreevich0711 <egorgrebennikov39@gmail.com>
2025-07-02 00:26:06 +0800

    Мои текущие изменения

--- internship/settings.py
+++ internship/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fzgk^8!_gxzjtrg#q!e4^k-3)5gtk=6d8pcm@jk22pjtvhs086'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mountain_pass'
+    'rest_framework',
+    'mountain_pass',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +53,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ],
+    'DEFAULT_CHARSET': 'utf-8',
+}
 
 ROOT_URLCONF = 'internship.urls'
 
@@ -75,8 +92,16 @@ WSGI_APPLICATION = 'internship.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Pereval',
+        'USER': os.getenv('FSTR_DB_LOGIN'),
+        'PASSWORD': os.getenv('FSTR_DB_PASS'),
+        'HOST': os.getenv('FSTR_DB_HOST'),
+        'PORT': os.getenv('FSTR_DB_PORT'),
+        'OPTIONS': {
+            'options': '-c search_path=public,other_schemas',
+            'client_encoding': 'UTF8',
+        },
     }
 }
 
@@ -121,3 +146,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_CHARSET = 'utf-8'
+FILE_CHARSET = 'utf-8'
--- mountain_pass/urls.py
+++ mountain_pass/urls.py
@@ -2,11 +2,7 @@ from django.urls import path
 from .views import SubmitDataView,  UserMountainPassList, MountainPassDetailUpdate
 
 urlpatterns = [
-
     path('submitData/', SubmitDataView.as_view(), name='submit-data'),
-
     path('submitData/<int:pk>/', MountainPassDetailUpdate.as_view(), name='mountainpass-detail-update'),
-
-
     path('submitData/', UserMountainPassList.as_view(), name='mountainpass-user-list'),
 ]
